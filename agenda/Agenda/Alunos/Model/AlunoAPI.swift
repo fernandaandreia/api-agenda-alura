a04f9909c37f8fb4443a01a0b91e7979d3a3f2a5
fernandaandreia <fernanda.nascimento.btk@gmail.com>
2020-12-28 14:04:58 -0300

    recuperando-alunos-servidor

--- agenda/Agenda/Alunos/Model/AlunoAPI.swift
+++ agenda/Agenda/Alunos/Model/AlunoAPI.swift
@@ -11,6 +11,27 @@ import Alamofire
 
 class AlunoAPI: NSObject {
 
+    // MARK: - GET
+    func recuperaAlunos(){
+        Alamofire.request("http://localhost:8080/api/aluno/lista", method: .get).responseJSON { (response) in
+            
+            switch response.result {
+            case .success:
+                print(response.result.value!)
+                
+            break
+            case .failure:
+                print(response.error!)
+                break
+    
+            }
+        }
+            
+        
+    }
+    
+    
+    // MARK: - PUT
     func salvaAlunosNoServidor(parametros: Array<Dictionary<String, String>>) {
         guard let url = URL (string: "http://localhost:8080/api/aluno/lista") else { return }
         var requisicao = URLRequest(url: url)
